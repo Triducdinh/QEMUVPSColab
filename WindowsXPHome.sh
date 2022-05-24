14fedabe20ea3c3beb36f3c954883497e72086d9
QuocAnVN1310 <106017811+QuocAnVN1310@users.noreply.github.com>
2022-05-24 15:04:31 +0700

    Create WindowsXPHome.sh

---
+++ WindowsXPHome.sh
@@ -0,0 +1,25 @@
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
+unzip -o ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+rm -rf ngrok-stable-linux-amd64.zip
+read -p "Enter your ngrok token: " TOKEN
+./ngrok authtoken $TOKEN
+nohup ./ngrok tcp --region ap 5900 &>/dev/null &
+sudo apt-get update && apt-get install qemu -y
+sudo apt install qemu-utils -y
+sudo apt install qemu-system-x86-xen -y
+sudo apt install qemu-system-x86 -y
+qemu-img create -f raw windows.img 100G
+wget -O virtio-win.iso 'https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/archive-virtio/virtio-win-0.1.215-1/virtio-win-0.1.215.iso'
+wget -O windowsxphome.iso 'https://dl.bobpony.com/windows/xp/home/en_winxp_home_x86_build2600.iso'
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+sudo qemu-system-x86_64 \
+  -m 4G \
+  -cpu EPYC \
+  -boot order=d \
+  -drive file=windowsxphome.iso,media=cdrom \
+  -drive file=windows.img,format=raw \
+  -drive file=virtio-win.iso,media=cdrom \
+  -device usb-ehci,id=usb,bus=pci.0,addr=0x4 \
+  -device usb-tablet \
+  -vnc :0 \
+  -smp cores=2 
